88cbc851e9d8fba17a5e087aeaf2bb51ad06001a
Rohit Tiwari <rohitttiwari52@gmail.com>
2023-12-08 20:03:57 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 build() {
 source build/envsetup.sh || . build/envsetup.sh
-bash device/xiaomi/fleur/prebuilts/vendor.sh
 lunch $MAKEFILENAME-$VARIENT
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-ccache -M 100G
+ccache -M 50G
 ccache -o compression=true
 ccache -z
 $EXTRACMD

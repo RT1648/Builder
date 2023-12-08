2f565f51236321cfb6eb6d5b3bcf031443ab3c94
Rohit Tiwari <rohitttiwari52@gmail.com>
2023-12-08 20:14:59 +0530

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 setup() {
-sudo apt update &&sudo apt install pigz wget jq curl repo -y
+sudo apt update &&sudo apt install pigz wget jq curl -y
 sudo apt upgrade -y
 }
 echo "Setting Up AOSP Build Environment"
